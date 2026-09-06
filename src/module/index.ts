7df9b941f55a89c69919b17a4cec9837a23c98b6
Ebrahim khasal <webtanan@gmail.com>
2026-09-06 11:37:36 +0330

    fix(package): emit RACI module CSS

--- src/module/index.ts
+++ src/module/index.ts
@@ -1,1 +1,6 @@
-export{moduleManifest}from'../../module.manifest';export{RaciWorkspace,type RaciWorkspaceProps}from'../components/RaciWorkspace';export{TinyRaciRepository,KEY as TINY_RACI_STORAGE_KEY}from'../domain/repository';export type*from'../domain/types';
+import '../styles.css';
+
+export { moduleManifest } from '../../module.manifest';
+export { RaciWorkspace, type RaciWorkspaceProps } from '../components/RaciWorkspace';
+export { TinyRaciRepository, KEY as TINY_RACI_STORAGE_KEY } from '../domain/repository';
+export type * from '../domain/types';
